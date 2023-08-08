cb85ef75d987dbe2d941d72492358cb6ca9eb1df
Your Name <you@example.com>
2023-08-08 20:16:24 +1000

    Update classification model, add streamlit for prediction app

--- Model/streamlit_fhs.py
+++ Model/streamlit_fhs.py
@@ -2,37 +2,60 @@ import streamlit as st
 import joblib
 import pandas as pd
 
-st.write("# Attrition Rate")
+st.write("# Attrition Prediction")
 
 col1, col2, col3 = st.columns(3)
 
-# getting user inputgender = col1.selectbox("Enter your gender",["Male", "Female"])
+Age = col1.number_input("Enter your age")
+DistanceFromHome = col2.number_input("DistanceFromHome")
+MonthlyIncome = col3.number_input("Enter your Monthly Income in USD")
 
-Monthly_income = col1.number_input("Enter your Monthly Income in USD")
-age = col2.number_input("Enter your age")
-Overtime = col3.number_input("Enter your overtime hours")
+NumCompaniesWorked = col1.number_input("Enter your # of companies worked")
+OverTime = col2.selectbox("Overtime?",["Yes", "No"])
+PercentSalaryHike = col3.number_input("Enter your PercentSalaryHike")
 
-Daily_rate = col1.number_input("Enter your Daily Rate")
-Monthly_rate = col2.number_input("Enter your Monthly Rate")
-TotalWorkingYears = col3.number_input("Enter your Total working years")
+TotalWorkingYears = col1.number_input("Working years") 
+YearsAtCompany = col2.number_input("YearsAtCompany")
+YearsInCurrentRole = col3.number_input("YearsInCurrentRole")
 
-Hourly_rate = col1.number_input("Enter your Hourly Rate")
-YearsAtCompany= col2.number_input("Enter your Years at current company")
-NumCompaniesWorked = col3.number_input("Enter your number of companies worked")
+YearsWithCurrManager = col1.number_input("YearsWithCurrManager")
 
-PercentSalaryHike = col2.number_input("Enter your Percent Salary Hike")
-
-st.button('Predict')
-
-df_pred = pd.DataFrame([[Monthly_income,
-                         age,
-                         Overtime,
-                         Daily_rate,
-                         Monthly_rate,
-                         TotalWorkingYears,
-                         Hourly_rate,
-                         YearsAtCompany,
+df_pred = pd.DataFrame([[Age,
+                         DistanceFromHome,
+                         MonthlyIncome,
                          NumCompaniesWorked,
-                         PercentSalaryHike]])
+                         OverTime,
+                         PercentSalaryHike,
+                         TotalWorkingYears,
+                         YearsAtCompany,
+                         YearsInCurrentRole,                         
+                         YearsWithCurrManager]],
+
+              columns = ["Age",
+                         "DistanceFromHome",
+                         "MonthlyIncome",
+                         
+                         "NumCompaniesWorked",
+                         "OverTime",
+                         "PercentSalaryHike",
+                         "TotalWorkingYears",
+                         "YearsAtCompany",                         
+                         "YearsInCurrentRole",                        
+                         "YearsWithCurrManager"])                    
+
+df_pred['OverTime'] = df_pred['OverTime'].apply(lambda x: 1 if x == 'Yes' else 0)
+
+model = joblib.load('rf_clf_model.pkl')
+prediction = model.predict(df_pred)
+
+if st.button('Predict'):
+
+    if(prediction[0]==0):
+        st.write('<p class="big-font">Employee is likely to stay.</p>',unsafe_allow_html=True)
+        st.markdown("<style>.big-font { font-size: 40px; }</style>", unsafe_allow_html=True)
+        
+    else:
+        st.write('<p class="big-font">Employee is not likely to stay.</p>',unsafe_allow_html=True)
+        st.markdown("<style>.big-font { font-size: 40px; }</style>", unsafe_allow_html=True)  
 
 
